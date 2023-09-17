29dd117108e1241bb5febc03a3eb718515e33c1e
uimkatali <b8bf6b5@gmail.com>
2023-09-17 21:22:35 +0300

    Add project files.

---
+++ lab1ex6/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace lab1ex1
+{
+    class Program
+    {
+        /*Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele trei valori in ordine
+        descrescatoare.
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
